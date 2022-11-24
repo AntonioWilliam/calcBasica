609431d2e94ebcacd537a68609e5ad0f483bf73e
Antonio William Almeida <almeidawilliam241@gmail.com>
2022-11-24 02:50:16 -0300

    Primeiro envio

---
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.calculadorabasica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txt1;
+    private EditText txt2;
+    private TextView tvResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt1 = findViewById(R.id.txt1);
+        txt2 = findViewById(R.id.txt2);
+        tvResultado = findViewById(R.id.tvResultado);
+    }
+    public void somar(View view){
+        int valor1 = Integer.parseInt(txt1.getText().toString());
+        int valor2=Integer.parseInt(txt2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void subtrair(View view){
+        int valor1 = Integer.parseInt(txt1.getText().toString());
+        int valor2=Integer.parseInt(txt2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 - valor2));
+    }
+
+    public void mult(View view){
+        int valor1 = Integer.parseInt(txt1.getText().toString());
+        int valor2=Integer.parseInt(txt2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 * valor2));
+    }
+
+    public void dividir(View view){
+        int valor1 = Integer.parseInt(txt1.getText().toString());
+        int valor2=Integer.parseInt(txt2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 / valor2));
+    }
+}
